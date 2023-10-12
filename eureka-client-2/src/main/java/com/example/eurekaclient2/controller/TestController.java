e5e02536a46d8ea51fa7411b296e8b84931dc4fb
sepxx <mihartthx@gmail.com>
2023-10-12 14:10:09 +0300

    first commit

---
+++ eureka-client-2/src/main/java/com/example/eurekaclient2/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class TestController {
+
+    @GetMapping("/test2")
+    public String test() {
+        return "test2";
+    }
+}
